e0232c61490cdc95d931eff90483505c7c59be42
Thijs Raaijmakers <TRaaijmakers32@gmail.com>
2022-05-11 11:18:10 +0200

    Added few statussus

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -18,8 +18,8 @@ let controller = {
             assert(typeof city === 'string', 'City must be a string')
 
         } catch (error) {
-            res.status(400).json({
-                status: 400,
+            res.status(409).json({
+                status: 409,
                 message: error.message
             })
             next(err)
@@ -71,8 +71,8 @@ let controller = {
 
                 // Handle error after the release.
                 if (error) {
-                    res.status(404).json({
-                        status: 404,
+                    res.status(400).json({
+                        status: 400,
                         message: error.message
                     })
                 }
