aad5a94a623ae237e74d738c3ba4c1fb13ed5e51
Thijs Raaijmakers <TRaaijmakers32@gmail.com>
2022-05-11 21:14:58 +0200

    UC 201 tests done

--- test/integration/user.controller.test.js
+++ test/integration/user.controller.test.js
@@ -8,128 +8,70 @@ chai.use(chaiHttp)
 
 describe('Manage users /api/user', () => {
 
-    describe('UC-201 Register as a new user', () => {
-
-        //it('Required input is missing', (done) => { //TC-201-1
-        //    chai
-        //        .request(server)
-        //        .post('/api/user')
-        //        .send({
-        //            //"firstName": "Test", First name is missing
-        //            "lastName": "van den Test",
-        //            "isActive": 1,
-        //            "emailAdress": "testmail3462@gmail.com",
-        //            "password": "secret",
-        //            "phoneNumber": "test",
-        //            "roles": "editor,guest",
-        //            "street": "Lovensdijkstraat",
-        //            "city": "Breda"
-        //        })
-        //        .end((err, res) => {
-        //            res.should.be.an('object')
-        //            let { status, result } = res.body;
-        //            status.should.equal(404);
-        //            result.should.be.an('string').that.equals("Required input is missing")
-        //            done()
-        //        })
-        //})
-
-        it('Invalid emailAdress', (done) => { //TC-201-2
+    describe('UC-201 Registreren', () => {
+        it('TC-201-1 Verplicht velt ontbreekt', (done) => {
+            chai.request(server).post('/api/user').send({
+                //"firstName": "John",
+                lastName: "Doe",
+                isActive: 1,
+                emailAdress: "test@gmail.com",
+                password: "secret",
+                phoneNumber: "-",
+                roles: "guest",
+                street: "Lovensdijkstraat",
+                city: "Breda"
+            })
+            .end((err, res) => {
+                res.should.be.an('object');
+                let { status, message } = res.body;
+                status.should.equals(400)
+                message.should.be.a('string').that.equals('firstName must be a string');
+                done();
+            });
+        })
+        it('TC-201-4 Gebruiker bestaat al', (done) => {
             chai
-                .request(server)
-                .post('/api/user')
-                .send({
-                    "firstName": "Test",
-                    "lastName": "van den Test",
-                    "isActive": 1,
-                    "emailAdress": 6, //Invalid email address
-                    "password": "secret",
-                    "phoneNumber": "test",
-                    "roles": "editor,guest",
-                    "street": "Lovensdijkstraat",
-                    "city": "Breda"
-                })
-                .end((err, res) => {
-                    res.should.be.an('object')
-                    let { status, result } = res.body;
-                    status.should.equal(404);
-                    result.should.be.an('string').that.equals("Email address must be a string")
-                    done()
-                })
+            .request(server)
+            .post('/api/user')
+            .send({
+                "firstName": "Johannes",
+                "lastName": "Wiebel",
+                "emailAdress": "testmail@gmail.com",
+                "password": "secret",
+                "street": "test",
+                "city": "test"
+            })
+            .end((err, res) => {
+                res.should.be.an('object')
+                let { status, message } = res.body;
+                status.should.equal(409);
+                message.should.be.an('string');
+                done();
+            }) 
         })
 
-        it('Invalid password', (done) => { //TC-201-3
+        it('TC-201-5 Gebruiker succesvol geregistreerd', (done) => {
             chai
-                .request(server)
-                .post('/api/user')
-                .send({
-                    "firstName": "Test",
-                    "lastName": "van den Test",
-                    "isActive": 1,
-                    "emailAdress": "testmail3462@gmail.com",
-                    "password": 7,
-                    "phoneNumber": "test",
-                    "roles": "editor,guest",
-                    "street": "Lovensdijkstraat",
-                    "city": "Breda"
-                })
-                .end((err, res) => {
-                    res.should.be.an('object')
-                    let { status, result } = res.body;
-                    status.should.equal(404);
-                    result.should.be.an('string').that.equals("Password must be a string")
-                    done()
-                })
-        })
-
-        it('User already exists', (done) => { //TC-201-3
-            chai
-                .request(server)
-                .post('/api/user')
-                .send({
-                    "firstName": "Mariëtte",
-                    "lastName": "van den Dullemen",
-                    "isActive": 1,
-                    "emailAdress": "m.vandullemen@server.nl",
-                    "password": "secret",
-                    "phoneNumber": "",
-                    "roles": "",
-                    "street": "",
-                    "city": ""
-                })
-
-                .end((err, res) => {
-                    res.should.be.an('object')
-                    let { status, result } = res.body;
-                    status.should.equal(404);
-                    //result.should.be.an('string').that.equals("Password is invalid")
-                    done()
-                })
-        })
-
-        it('User successfully added', (done) => { //TC-201-3
-            chai
-                .request(server)
-                .post('/api/user')
-                .send({
-                    "firstName": "Sucessful",
-                    "lastName": "van den Test",
-                    "isActive": 1,
-                    "emailAdress": "testmail@gmail.com",
-                    "password": "secret",
-                    "phoneNumber": "test",
-                    "roles": "editor,guest",
-                    "street": "Lovensdijkstraat",
-                    "city": "Breda"
-                })
-                .end((err, res) => {
-                    res.should.be.an('object')
-                    let { status, result } = res.body;
-                    status.should.equal(200);
-                    //result.should.be.an('string').that.equals("User added successfully")
-                    done()
-                })
-        })
-        //it('When required input is missing, return an error', (done) => {}) More testcases
+            .request(server)
+            .post('/api/user')
+            .send({
+                "firstName": "Test",
+                "lastName": "Tester",
+                "isActive": 1,
+                "emailAdress": "newuser@gmail.com",
+                "password": "secret",
+                "phoneNumber": "-",
+                "roles": "guest",
+                "street": "Lovensdijkstraat",
+                "city": "Breda"
+            })
+            .end((err, res) => {
+                res.should.be.an('object')
+                let { status, result } = res.body;
+                status.should.equal(201);
+                result.should.be.an('object');
+                done();
+            }) 
+        });
     })
 })
