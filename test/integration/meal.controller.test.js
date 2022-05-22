4177e2c8b87697563b9942c55e390539f3103f86
Thijs Raaijmakers <TRaaijmakers32@gmail.com>
2022-05-22 22:22:00 +0200

    Added UC-303 tests

--- test/integration/meal.controller.test.js
+++ test/integration/meal.controller.test.js
@@ -179,102 +179,151 @@ describe('Meals', () => {
                 })
         }),
 
-        it('TC-302-2 Niet ingelogd', (done) => {
-            chai
-                .request(server)
-                .put('/api/meal/2')
-                .send({
-                    "isActive": 1,
-                    "isVega": 1,
-                    "isVegan": 0,
-                    "isToTakeHome": 0,
-                    "dateTime": "2020-05-20 00:40:00.000",
-                    "maxAmountOfParticipants": 5,
-                    "price": 4.20,
-                    "imageUrl": "https://images0.persgroep.net/rcs/MbMRv6NyAxtAIhKmT4_pYZstOuY/diocontent/115445912/_fitwidth/694/?appId=21791a8992982cd8da851550a453bd7f&quality=0.8",
-                    "name": "fries",
-                    "description": "tasty fries",
-                    "allergenes": ""
-                })
-                .end((err, res) => {
-                    res.should.be.an('object')
-                    let { status, result } = res.body;
-                    status.should.equal(401);
-                    result.should.be.an('string').that.equals("No key found");
-                    done();
-                })
-        }),
+            it('TC-302-2 Niet ingelogd', (done) => {
+                chai
+                    .request(server)
+                    .put('/api/meal/2')
+                    .send({
+                        "isActive": 1,
+                        "isVega": 1,
+                        "isVegan": 0,
+                        "isToTakeHome": 0,
+                        "dateTime": "2020-05-20 00:40:00.000",
+                        "maxAmountOfParticipants": 5,
+                        "price": 4.20,
+                        "imageUrl": "https://images0.persgroep.net/rcs/MbMRv6NyAxtAIhKmT4_pYZstOuY/diocontent/115445912/_fitwidth/694/?appId=21791a8992982cd8da851550a453bd7f&quality=0.8",
+                        "name": "fries",
+                        "description": "tasty fries",
+                        "allergenes": ""
+                    })
+                    .end((err, res) => {
+                        res.should.be.an('object')
+                        let { status, result } = res.body;
+                        status.should.equal(401);
+                        result.should.be.an('string').that.equals("No key found");
+                        done();
+                    })
+            }),
 
-        it('TC-302-3 Niet de eigenaar van de data', (done) => {
-            chai
-                .request(server)
-                .put('/api/meal/3')
-                .send({
-                    "isActive": 1,
-                    "isVega": 1,
-                    "isVegan": 0,
-                    "isToTakeHome": 0,
-                    "dateTime": "2020-05-20 00:40:00.000",
-                    "maxAmountOfParticipants": 5,
-                    "price": 4.20,
-                    "imageUrl": "https://images0.persgroep.net/rcs/MbMRv6NyAxtAIhKmT4_pYZstOuY/diocontent/115445912/_fitwidth/694/?appId=21791a8992982cd8da851550a453bd7f&quality=0.8",
-                    "name": "fries",
-                    "description": "tasty fries",
-                    "allergenes": ""
-                })
-                .end((err, res) => {
-                    res.should.be.an('object')
-                    let { status, result } = res.body;
-                    status.should.equal(401);
-                    result.should.be.an('string').that.equals("No key found");
-                    done();
-                })
-        }),
+            it('TC-302-3 Niet de eigenaar van de data', (done) => {
+                chai
+                    .request(server)
+                    .put('/api/meal/3')
+                    .send({
+                        "isActive": 1,
+                        "isVega": 1,
+                        "isVegan": 0,
+                        "isToTakeHome": 0,
+                        "dateTime": "2020-05-20 00:40:00.000",
+                        "maxAmountOfParticipants": 5,
+                        "price": 4.20,
+                        "imageUrl": "https://images0.persgroep.net/rcs/MbMRv6NyAxtAIhKmT4_pYZstOuY/diocontent/115445912/_fitwidth/694/?appId=21791a8992982cd8da851550a453bd7f&quality=0.8",
+                        "name": "fries",
+                        "description": "tasty fries",
+                        "allergenes": ""
+                    })
+                    .end((err, res) => {
+                        res.should.be.an('object')
+                        let { status, result } = res.body;
+                        status.should.equal(401);
+                        result.should.be.an('string').that.equals("No key found");
+                        done();
+                    })
+            }),
 
-        it('TC-302-4 Maaltijd bestaat niet', (done) => {
-            chai
-                .request(server)
-                .put('/api/meal/10')
-                .send({
-                    "isActive": 1,
-                    "isVega": 1,
-                    "isVegan": 0,
-                    "isToTakeHome": 0,
-                    "dateTime": "2020-05-20 00:40:00.000",
-                    "maxAmountOfParticipants": 5,
-                    "price": 4.20,
-                    "imageUrl": "https://images0.persgroep.net/rcs/MbMRv6NyAxtAIhKmT4_pYZstOuY/diocontent/115445912/_fitwidth/694/?appId=21791a8992982cd8da851550a453bd7f&quality=0.8",
-                    "name": "fries",
-                    "description": "tasty fries",
-                    "allergenes": ""
-                })
-                .end((err, res) => {
-                    res.should.be.an('object')
-                    let { status, result } = res.body;
-                    status.should.equal(404);
-                    result.should.be.an('string').that.equals("Meal by id 10 does not exist");
-                    done();
-                })
-        }),
+            it('TC-302-4 Maaltijd bestaat niet', (done) => {
+                chai
+                    .request(server)
+                    .put('/api/meal/10')
+                    .send({
+                        "isActive": 1,
+                        "isVega": 1,
+                        "isVegan": 0,
+                        "isToTakeHome": 0,
+                        "dateTime": "2020-05-20 00:40:00.000",
+                        "maxAmountOfParticipants": 5,
+                        "price": 4.20,
+                        "imageUrl": "https://images0.persgroep.net/rcs/MbMRv6NyAxtAIhKmT4_pYZstOuY/diocontent/115445912/_fitwidth/694/?appId=21791a8992982cd8da851550a453bd7f&quality=0.8",
+                        "name": "fries",
+                        "description": "tasty fries",
+                        "allergenes": ""
+                    })
+                    .end((err, res) => {
+                        res.should.be.an('object')
+                        let { status, result } = res.body;
+                        status.should.equal(404);
+                        result.should.be.an('string').that.equals("Meal by id 10 does not exist");
+                        done();
+                    })
+            }),
 
-        it('TC-302-5 Maaltijd succesvol gewijzigd', (done) => {
+            it('TC-302-5 Maaltijd succesvol gewijzigd', (done) => {
+                chai
+                    .request(server)
+                    .put('/api/meal/2')
+                    .set({ 'authorization': key })
+                    .send({
+                        "isActive": 1,
+                        "isVega": 1,
+                        "isVegan": 0,
+                        "isToTakeHome": 0,
+                        "dateTime": "2020-05-20 00:40:00.000",
+                        "maxAmountOfParticipants": 5,
+                        "price": 4.20,
+                        "imageUrl": "https://images0.persgroep.net/rcs/MbMRv6NyAxtAIhKmT4_pYZstOuY/diocontent/115445912/_fitwidth/694/?appId=21791a8992982cd8da851550a453bd7f&quality=0.8",
+                        "name": "fries",
+                        "description": "tasty fries",
+                        "allergenes": ""
+                    })
+                    .end((err, res) => {
+                        res.should.be.an('object')
+                        let { status, result } = res.body;
+                        status.should.equal(200);
+                        result.should.be.an('array');
+                        done();
+                    })
+            })
+    });
+
+    describe('UC-303 Lijst van alle maaltijden opvragen', () => {
+        beforeEach((done) => {
+            pools.getConnection(function (err, connection) {
+                connection.query(CLEAR_MEAL_TABLE, function (error, results, fields) {
+                    if (error) console.log(error);
+                    connection.query(CLEAR_PARTICIPANTS_TABLE, function (error, results, fields) {
+                        if (error) console.log(error);
+                        connection.query(CLEAR_USERS_TABLE, function (error, results, fields) {
+                            if (error) console.log(error);
+                            connection.query(`INSERT INTO user (id,firstName, lastName, isActive, emailAdress, password, phoneNumber, roles, street, city) VALUES
+                            (5,"testname","test",0,"testmail2@gmail.com","secret","test","guest","test","test")`, function (error, results, fields) {
+                                if (error) throw error;
+                                connection.query(`INSERT INTO user (id,firstName, lastName, isActive, emailAdress, password, phoneNumber, roles, street, city) VALUES
+                                (1,"testname","test", 0,"testmail@gmail.com","secret","test","guest","test","test")`, function (error, results, fields) {
+                                    if (error) throw error;
+                                    connection.query(`INSERT INTO meal (id,isActive, isVega, isVegan, isToTakeHome, dateTime, maxAmountOfParticipants, price, imageUrl, cookId, name, description, allergenes) VALUES
+                                    (2,0,0,0,0, "2020-05-20 00:40:00.000", 5, 4.20, "https://images0.persgroep.net/rcs/MbMRv6NyAxtAIhKmT4_pYZstOuY/diocontent/115445912/_fitwidth/694/?appId=21791a8992982cd8da851550a453bd7f&quality=0.8",
+                                    1,"fries","tasty fries","gluten")`, function (error, results, fields) {
+                                        if (error) throw error;
+                                        connection.query(`INSERT INTO meal (id,isActive, isVega, isVegan, isToTakeHome, dateTime, maxAmountOfParticipants, price, imageUrl, cookId, name, description, allergenes) VALUES
+                                        (3,0,0,0,0, "2020-05-20 00:40:00.000", 5, 4.20, "https://images0.persgroep.net/rcs/MbMRv6NyAxtAIhKmT4_pYZstOuY/diocontent/115445912/_fitwidth/694/?appId=21791a8992982cd8da851550a453bd7f&quality=0.8",
+                                        5,"fries","tasty fries","gluten")`, function (error, results, fields) {
+                                            if (error) throw error;
+                                            connection.release();
+                                            done();
+                                        })
+                                    })
+                                });
+                            });
+                        });
+                    });
+                });
+            })
+        });
+
+        it('TC-303-1 Lijst van alle maaltijden teruggegeven', (done) => {
             chai
                 .request(server)
-                .put('/api/meal/2')
-                .set({ 'authorization': key })
-                .send({
-                    "isActive": 1,
-                    "isVega": 1,
-                    "isVegan": 0,
-                    "isToTakeHome": 0,
-                    "dateTime": "2020-05-20 00:40:00.000",
-                    "maxAmountOfParticipants": 5,
-                    "price": 4.20,
-                    "imageUrl": "https://images0.persgroep.net/rcs/MbMRv6NyAxtAIhKmT4_pYZstOuY/diocontent/115445912/_fitwidth/694/?appId=21791a8992982cd8da851550a453bd7f&quality=0.8",
-                    "name": "fries",
-                    "description": "tasty fries",
-                    "allergenes": ""
-                })
+                .get('/api/meal')
                 .end((err, res) => {
                     res.should.be.an('object')
                     let { status, result } = res.body;
